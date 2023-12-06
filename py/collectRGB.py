57bfa310b193d709c11a00e18791fb907c1e5275
kapakino <leshiiiee66@gmail.com>
2023-12-06 22:32:12 +0800

    advise the python collect script

--- py/collectRGB.py
+++ py/collectRGB.py
@@ -2,9 +2,17 @@ from PIL import Image
 
 def collect(path:str):
     im = Image.open(f'{path}','r')
+    if im.mode != 'RGB':    im.convert('RGB')
     width,height = im.size
-    return list(im.getdata()),width,height
-
+    pixels = []
+    for i in range(height):
+        for j in range(width):
+            pixels.append(im.getpixel((j,i)))
+    return pixels,width,height
 if __name__ == '__main__':
+    #this collect the collor not white
     test,_,_ = collect('../test/test.jpg')
-    print(test)
+    for it in test:
+        if it==(255,255,255):
+            pass
+        else: print(it)
