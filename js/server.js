88416f96dea51722e8b3db8c1da104a569140de2
kapakino <leshiiiee66@gmail.com>
2024-01-04 15:42:21 +0800

    still exisit the bug of packed electron can't fetch data

--- js/server.js
+++ js/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 //allow to use await keyword
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const spawn = require('child_process').spawn;
 //with cors to access different domain
 const cors = require('cors');
 // const path = require('node:path');
@@ -13,8 +14,12 @@ const fs = require('fs')
 const ImgControl = require('./imageGenerate.js');
 
 
+//global
 const expressApplication = express();
 const port = 8080;
+
+var cmdProcess = undefined;
+var linuxProcess = undefined;
 
 //10MB
 const maxBuffer = 10*1024*1024;
@@ -43,10 +48,10 @@ expressApplication.post('/running',async (req,res)=>{
     }
     try{
         var imageData = undefined;
-        var cur_dir = __dirname;
-        process.chdir(`${__dirname}\\..\\py`);
+        // var cur_dir = __dirname;
+        // process.chdir(`${__dirname}\\..\\py`);
         res.on('finish',()=>{
-            process.chdir(cur_dir);
+            // process.chdir(cur_dir);
             //delete the file
             fs.unlink(`${__dirname}\\image\\output.jpg`,(error)=>{
                 if(error)console.error(`${error}`);
@@ -58,23 +63,66 @@ expressApplication.post('/running',async (req,res)=>{
                 .catch((error)=>{throw new Error(`${error}`)});
             },100);
         });
-        await exec(`python control.py ${arr}`,{maxBuffer},(error,stdout,stderr)=>{
-            if(error){
-                res.status(500).json({'error':error.message});
-                throw new Error(`Can't execute python script
-                error: ${error}
-                stderr:${stderr}`);
-            }else{
-                var idx = stdout.indexOf('PythonOutput:');
-                if(idx===-1)throw new Error('Can\'t read the output data');
+        //  The exec version of execute python script
+        // 
+        // const ConnectToPython = async()=>{
+        //     await exec(`python control.py ${arr}`,{maxBuffer},(error,stdout,stderr)=>{
+        //         if(error){
+        //             res.status(500).json({'error':error.message});
+        //             throw new Error(`Can't execute python script
+        //             error: ${error}
+        //             stderr:${stderr}`);
+        //         }else{
+        //             var idx = stdout.indexOf('PythonOutput:');
+        //             if(idx===-1)throw new Error('Can\'t read the output data');
+        //             //PythonOutput: has 13 characters
+        //             var data = stdout.split('').slice(idx+13).join('');
+        //             // console.log(data);
+        //             imageData = new ImgControl(data);
+                    
+        //             res.json({'output':data});
+        //         }
+        //     });
+        // }
+        if(process.platform.includes('win')){
+            cmdProcess = spawn('cmd.exe',['/K']);
+            cmdProcess.stdout.on('data',(data)=>{
+                data = data.toString();
+                // console.log('Enter',data);
+                // console.log(`stdout:${data}`);
+                var idx = data.indexOf('PythonOutput:');
+                // if(idx===-1)throw new Error('Can\'t read the output data');
+                if(idx===-1)return;
                 //PythonOutput: has 13 characters
-                var data = stdout.split('').slice(idx+13).join('');
+                var output = data.split('').slice(idx+13).join('');
                 // console.log(data);
-                imageData = new ImgControl(data);
+                imageData = new ImgControl(output);
                 
-                res.json({'output':data});
-            }
-        });
+                res.json({'output':output});
+            })
+            cmdProcess.on('error',(error)=>{
+                console.log(`Error:${error}`);
+            })
+            cmdProcess.stdin.write(`cd py\r\n`)
+            cmdProcess.stdin.write(`python control.py ${arr}\r\n`);
+        }else{
+            linuxProcess = spawn('xterm',['-e',`cd py & python control.py ${arr}`]);
+            linuxProcess.stdout.on('data',(data)=>{
+                data = data.toString();
+                var idx = data.indexOf('PythonOutput:');
+                // if(idx===-1)throw new Error('Can\'t read the output data');
+                if(idx===-1)return;
+                //PythonOutput: has 13 characters
+                var output = data.split('').slice(idx+13).join('');
+                // console.log(data);
+                imageData = new ImgControl(output);
+                
+                res.json({'output':output});
+            });
+            linuxProcess.on('error',(error)=>{
+                console.log(`Error:${error}`);
+            })
+        }
     }catch(error){
         console.log(`Error:${error}`)
     }
